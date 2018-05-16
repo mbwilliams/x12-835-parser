177e31c575c7264f6576aa44ea8839e36a080ba7
Marlon Williams <marlon.willi@gmail.com>
2018-05-16 10:35:21 -0500

    Finalize spec and Implement N4 segment class

--- spec/X221/n4_spec.rb
+++ spec/X221/n4_spec.rb
@@ -3,7 +3,7 @@ require 'X221/n4'
 module X221
   describe N4 do
     before do
-      @raw_data = 'N4*KANSAS CITY*MO*64108~'
+      @raw_data = 'N4*Toronto*Ontario*64108*CAD*YYY~'
       @n4 = N4.new(@raw_data)
     end
 
@@ -16,11 +16,11 @@ module X221
     end
 
     it "has a City Name" do
-      expect(@n4.city_name).to eq('KANSAS CITY')
+      expect(@n4.city_name).to eq('Toronto')
     end
 
     it "has a State or Province Code" do
-      expect(@n4.state_or_province_code).to eq('MO')
+      expect(@n4.state_or_province_code).to eq('Ontario')
     end
 
     it "has a Postal Code" do
@@ -28,11 +28,26 @@ module X221
     end
 
     it "has a Country Code" do
-      expect(@n4.country_code).to be_nil
+      expect(@n4.country_code).to eq('CAD')
     end
 
     it "has a Country Subdivision Code" do
-      expect(@n4.country_subdivision_code).to be_nil
+      expect(@n4.country_subdivision_code).to eq('YYY')
+    end
+
+    context "with no country information" do
+      before do
+        @raw_data = 'N4*KANSAS CITY*MO*64108~'
+        @n4 = N4.new(@raw_data)
+      end
+
+      it "has no Country Code" do
+        expect(@n4.country_code).to be_nil
+      end
+
+      it "has no Country Subdivision Code" do
+        expect(@n4.country_subdivision_code).to be_nil
+      end
     end
   end
 end
